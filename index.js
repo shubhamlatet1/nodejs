ef2189268478fdeb2916c76d4d7c9d010066a36c
shubhamlatet1@gmail.com <shubhamlatet1@gmail.com>
2022-12-28 10:54:13 +0530

    manogo

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
+import mongoose from 'mongoose';
 import router from './router/homeRouter.js';
-import connectDb from './db/connectDb.js';
 
 const app = express();
 const port = process.env.PORT || '8000';
 const DATABASE_URL = process.env.DATABASE_URI;
 
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(DATABASE_URL);
+    console.log(`MongoDB Connected`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-
-connectDb(DATABASE_URL);
 
 app.use('/', router);
 app.use('/detail', router);
 app.use('/signIn', router);
 app.use('/singleproduct', router);
 
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+connectDB().then(() => {
+  app.listen(port, () => {
+    console.log('listening for requests');
+  });
 });
